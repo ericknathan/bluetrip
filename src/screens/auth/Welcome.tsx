6287b3875498d379e0c2d3c92fe83655f40957eb
ericknathan <erick.capito@hotmail.com>
2024-05-29 20:38:46 -0300

    feat: create welcome screen

--- src/screens/auth/Welcome.tsx
+++ src/screens/auth/Welcome.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
-import { Header, Text } from "@/components";
+import { Button, Header, Text } from "@/components";
+import type { ScreenProps } from "@/navigation";
+import { theme } from "@/styles";
 
-export function WelcomeScreen() {
+export function WelcomeScreen({ navigation }: ScreenProps<"SignIn">) {
   return (
     <View style={{ flex: 1 }}>
       <Header />
-
       <View style={styles.container}>
-        <Text>Welcome</Text>
+        <Image source={require("@/assets/welcome.png")} style={styles.image} />
+        <Text
+          weight="semibold"
+          size={32}
+          align="center"
+          style={{ paddingHorizontal: 32 }}
+        >
+          Olá, boas vindas ao{" "}
+          <Text color={theme.primary[500]} size={32} weight="semibold">
+            Bluetrip
+          </Text>
+          !
+        </Text>
+        <Text size={18} align="center" color={theme.zinc[600]}>
+          Lorem ipsum dolor sit amet consectetur. Mi urna cras amet consequat
+          odio venenatis ipsum.
+        </Text>
+        <Button
+          style={{ width: "100%" }}
+          onPress={() => navigation.navigate("SignIn")}
+        >
+          Fazer login
+        </Button>
+        <Button
+          style={{ width: "100%" }}
+          variant="secondary"
+          onPress={() => navigation.navigate("SignUp")}
+        >
+          Criar nova conta
+        </Button>
       </View>
     </View>
   );
@@ -20,5 +50,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 20,
+    gap: 12,
+  },
+  image: {
+    width: "100%",
+    height: 300,
   },
 });
