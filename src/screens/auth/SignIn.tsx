822f9abe922ef5d5972644a6238fa510320571a7
ericknathan <erick.capito@hotmail.com>
2024-05-29 20:39:01 -0300

    feat: create signin screen

--- src/screens/auth/SignIn.tsx
+++ src/screens/auth/SignIn.tsx
@@ -1,24 +1,71 @@
-import { StyleSheet, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-import { Header, Text } from "@/components";
+import { Button, Header, Input, Text } from "@/components";
+import type { ScreenProps } from "@/navigation";
+import { theme } from "@/styles";
 
-export function SignInScreen() {
+export function SignInScreen({ navigation }: ScreenProps<"SignIn">) {
+  function handleSignIn() {
+    navigation.navigate("TouristSpots");
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Header />
+    <ScrollView style={{ flex: 1 }}>
+      <Header showBackButton />
 
       <View style={styles.container}>
-        <Text>SignIn</Text>
+        <Image
+          source={require("@/assets/auth.png")}
+          style={{ width: "100%", height: 200 }}
+          resizeMode="contain"
+        />
+        <Text weight="semibold" size={32}>
+          Entre em sua conta
+        </Text>
+        <Input
+          label="E-mail"
+          placeholder="alan.turing@email.com"
+          keyboardType="email-address"
+          autoComplete="email"
+          autoFocus
+        />
+        <Input
+          label="Senha"
+          placeholder="••••••••"
+          secureTextEntry
+          autoCapitalize="none"
+          autoComplete="current-password"
+        />
+        <TouchableOpacity style={{ paddingVertical: 12 }}>
+          <Text
+            color={theme.primary[500]}
+            style={{ textDecorationLine: "underline" }}
+          >
+            Esqueci a senha
+          </Text>
+        </TouchableOpacity>
+        <Button onPress={handleSignIn}>Fazer login</Button>
+        <Button
+          onPress={() => navigation.navigate("SignUp")}
+          variant="secondary"
+        >
+          Criar nova conta
+        </Button>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
+    padding: 20,
+    gap: 8,
   },
 });
