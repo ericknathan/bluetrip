046320c8ce8eb8f53677d030e8b71d0d5e618875
ericknathan <erick.capito@hotmail.com>
2024-05-29 20:39:09 -0300

    feat: create signup screen

--- src/screens/auth/SignUp.tsx
+++ src/screens/auth/SignUp.tsx
@@ -1,24 +1,92 @@
-import { StyleSheet, View } from "react-native";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 
-import { Header, Text } from "@/components";
+import { Button, Header, Input, Label, SelectGroup, Text } from "@/components";
+import { CountrySelector } from "@/components/CountrySelector";
+import type { ScreenProps } from "@/navigation";
 
-export function SignUpScreen() {
+export function SignUpScreen({ navigation }: ScreenProps<"SignUp">) {
+  function handleSignUp() {
+    navigation.navigate("SignIn");
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Header />
+    <ScrollView style={{ flex: 1 }}>
+      <Header showBackButton />
 
       <View style={styles.container}>
-        <Text>SignUp</Text>
+        <Image
+          source={require("@/assets/auth.png")}
+          style={{ width: "100%", height: 200 }}
+          resizeMode="contain"
+        />
+        <Text weight="semibold" size={32}>
+          Cadastre sua conta
+        </Text>
+        <Input
+          label="Nome completo"
+          placeholder="Alan turing Oliveira"
+          autoComplete="name"
+          autoFocus
+        />
+        <View>
+          <Label>Nacionalidade</Label>
+          <CountrySelector />
+        </View>
+        <Input
+          label="Telefone"
+          placeholder="(••) ••••• ••••"
+          autoComplete="tel"
+        />
+        <Input
+          label="Data de nascimento"
+          placeholder="__/__/____"
+          autoComplete="birthdate-day"
+        />
+        <View>
+          <Label>Gênero</Label>
+          <SelectGroup
+            options={[
+              { label: "Masculino", value: "m" },
+              { label: "Feminino", value: "f" },
+            ]}
+          />
+        </View>
+        <View>
+          <Label>Idioma</Label>
+          <SelectGroup
+            options={[
+              { label: "Português", value: "pt" },
+              { label: "Inglês", value: "en" },
+              { label: "Espanhol", value: "es" },
+            ]}
+          />
+        </View>
+        <Input
+          label="E-mail"
+          placeholder="alan.turing@email.com"
+          keyboardType="email-address"
+          autoComplete="email"
+        />
+
+        <Input
+          label="Senha"
+          placeholder="••••••••"
+          secureTextEntry
+          autoCapitalize="none"
+          autoComplete="current-password"
+        />
+        <Button onPress={handleSignUp} style={{ marginTop: 12 }}>
+          Criar conta
+        </Button>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
+    padding: 20,
+    gap: 8,
   },
 });
