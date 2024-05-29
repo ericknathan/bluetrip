4cb3e79e62aaa5d069a9b05a914679eb3e06efd9
ericknathan <erick.capito@hotmail.com>
2024-05-29 10:55:30 -0300

    feat: add theme and font family

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
+import { useCallback } from "react";
+import { View } from "react-native";
+
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from "@expo-google-fonts/poppins";
 import { NavigationContainer } from "@react-navigation/native";
-import { Router } from "./src/screens";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 
+import { theme } from "@/styles";
+import { Router } from "./src/screens";
+
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    text_regular: Poppins_400Regular,
+    text_semibold: Poppins_600SemiBold,
+    text_bold: Poppins_700Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <Router />
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <View
+      style={{ flex: 1, backgroundColor: theme.zinc[50] }}
+      onLayout={onLayoutRootView}
+    >
+      <NavigationContainer>
+        <Router />
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </View>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+  white: "#FFFFFF",
+  black: "#000000",
+  primary: {
+    500: "#1746A2",
+    700: "#122859",
+  },
+  zinc: {
+    50: "#FAFAFA",
+    100: "#F4F4F5",
+    200: "#E4E4E7",
+    300: "#D4D4D8",
+    400: "#A1A1AA",
+    500: "#71717A",
+    600: "#52525B",
+    700: "#3F3F46",
+    800: "#27272A",
+    900: "#18181B",
+    950: "#09090B",
+  },
+  green: {
+    500: "#22C55E",
+  },
+  red: {
+    500: "#EF4444",
+    600: "#dc2626",
+  },
+  border: "#FFFFFF1A",
+};
+
+export const fontFamily = {
+  regular: "text_regular",
+  semibold: "text_semibold",
+  bold: "text_bold",
+};
